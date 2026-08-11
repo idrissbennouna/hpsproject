2dc697257ff9e589a6f684227237e0c9c95cf387
echch <echchyoughi.mohamed@ensam-casa.ma>
2026-08-11 11:49:47 +0100

    removing the debug files

--- backend/app/services/debug_check_fields.py
+++
@@ -1,9 +0,0 @@
-# backend/app/services/debug_check_fields.py
-from app.rag.retriever import get_vectorstore
-
-db = get_vectorstore()
-docs = db.similarity_search("Field 2 Primary Account Number", k=1, filter={"type": "field_definition"})
-
-for doc in docs:
-    print(doc.metadata)
-    print(doc.page_content[:800])
